70dd80d457ad7b1dbe3e7a6bb9235c0b0467ed17
299hannah <hannah.wambui@student.moringaschool.com>
2021-07-03 17:15:04 +0300

    adding my context in the view

--- photos/views.py
+++ photos/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from .models import Category, Photo
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    categories = Category.objects.all()
+    photos = Photo.objects.all()
+
+    context = {'categories': categories, 'photos':photos}
+    
+    return render(request, 'index.html', context)
 
 def photos(request, pk):
-    return render(request, 'photos.html')
+    photo = Photo.objects.get(id=pk)
+
+    return render(request, 'photos.html',{'photo':photo})
