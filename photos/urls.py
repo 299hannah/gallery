6c08514ff4e01c226484e5c5a1109b39bb40f133
299hannah <hannah.wambui@student.moringaschool.com>
2021-07-04 12:30:36 +0300

    search url

--- photos/urls.py
+++ photos/urls.py
@@ -7,6 +7,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('',views.index, name='index'),
     path('photo/<str:pk>/',views.photos, name='photo'),
+    path('search/', views.search_results, name='search_results'),
+
 
 ]
 if settings.DEBUG:
