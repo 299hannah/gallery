feb1dd5ba8105ba4a8b7760af5b7e535cfc27d96
299hannah <hannah.wambui@student.moringaschool.com>
2021-07-04 12:30:22 +0300

    testing

--- photos/models.py
+++ photos/models.py
@@ -7,13 +7,30 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+
+    class Meta:
+        ordering = ['name']
+
+
 
 class Photo(models.Model):
     category = models.ForeignKey(Category,on_delete=models.SET_NULL, null=True, blank=True)
     image = models.ImageField(null=False, blank=False)
     description = models.TextField()
+    location = models.TextField()
 
     def __str__(self):
         return self.description
 
+    @classmethod
+    def search_by_location(cls,search_term):
+        photos = cls.objects.filter(location__icontains=search_term)
+        return photos
+    
 
