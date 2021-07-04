93355cfefee11dc3e37924ab57c28ce08fd55fe3
299hannah <hannah.wambui@student.moringaschool.com>
2021-07-05 01:59:02 +0300

    set

--- mygallery/settings.py
+++ mygallery/settings.py
@@ -11,17 +11,17 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
-import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =  'django-insecure-&30g&l-+00j_@7fmbq=l!&xpsqt$&d+s6ipfn_6#i4mes#)m8v'
+SECRET_KEY = os.environ.get("KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
